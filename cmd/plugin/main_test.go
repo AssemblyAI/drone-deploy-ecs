affd7ee6b21a41e2aee954ef5bec6b1ae4e37181
mitchya1 <mitch@mitchya1.com>
2021-05-14 13:48:03 -0600

    ensure required vars are set

--- cmd/plugin/main_test.go
+++ cmd/plugin/main_test.go
@@ -1,7 +1,33 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"gotest.tools/assert"
+)
 
 func TestNewECSClient(t *testing.T) {
 	newECSClient("us-east-2")
+}
+
+func TestCheckEnvVarsAllVarsSet(t *testing.T) {
+	os.Setenv("PLUGIN_AWS_REGION", "us-east-2")
+	os.Setenv("PLUGIN_SERVICE", "some-service")
+	os.Setenv("PLUGIN_CLUSTER", "some-cluster")
+	os.Setenv("PLUGIN_CONTAINER", "some-container-name")
+	os.Setenv("PLUGIN_IMAGE", "some/image:with-tag")
+
+	err := checkEnvVars()
+
+	assert.Equal(t, nil, err)
+}
+
+func TestCheckEnvVarsMissing(t *testing.T) {
+	os.Unsetenv("PLUGIN_IMAGE")
+
+	err := checkEnvVars()
+
+	assert.Error(t, err, "env var not set")
+
 }
